767bb95dbd5aa597d93f2f2548a64342874d2081
wjf990711 <49099966+wjf990711@users.noreply.github.com>
2020-06-02 13:23:15 +0800

    Updata2020/6/2 13:23

--- cin.cpp
+++ cin.cpp
@@ -7,11 +7,16 @@ class student3
 
 		int test_3()
 		{
-			//整型变量赋值
-			int b = 0;
-			cout << "请给整型变量b赋值：" << endl;
+			//整型变量赋值(浮点型，字符型，字符串型，布尔型同理)
+			double b = 0;
+			double c = 0;
+			cout << "请给整型被除数b赋值：" << endl;
 			cin >> b;
 			cout << "b = " << b << endl;
+			cout << "请给整型除数c赋值：" << endl;
+			cin >> c;
+			cout << "c = " << c << endl;
+			cout << "b / c = " << b / c << endl;
 
 
 			return 0;
