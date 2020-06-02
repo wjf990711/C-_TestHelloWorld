879d75b7713526b84e8722c0b944e48ebaf0cc82
wjf990711 <49099966+wjf990711@users.noreply.github.com>
2020-06-02 10:55:50 +0800

    Initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//常量定义方式1
+//#define 宏常量
+//常量定义方式2
+//const修饰的变量
+//在给变量起名时不要使用C++的关键字
+
+#define Day 7
+
+int main()
+{
+	int a;
+	const int month = 12;	//const修饰的变量被称为常量
+
+	a = 10;
+
+	//cout << Day << "天" << endl;
+	//cout << month << "月" << endl;
+
+
+	cout << "a = " << a << endl;
+
+	system("pause");
+
+	return 0;
+}
